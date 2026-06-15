e5f75bf6acc5defeba2d2cc1d6ac0bdfec468073
Chelo <marvillo028@gmail.com>
2026-06-14 18:43:43 -0600

    parseo de string input

---
+++ src/json_paseo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <cjson/cJSON.h>
+
+
+// creo que hay que incluir esto al archivo .h
+struct pelicula {
+    char *titulo;
+    char *descripcion;
+    char *calificacion;
+    char *poster_path;
+};
+
+int parsear_datos(char *string, struct pelicula *salida){
+
+    // parsea los datos
+
+    cJSON *json = cJSON_Parse(string);
+    if (json == NULL) {
+        const char *error_ptr = cJSON_GetErrorPtr();
+        if (error_ptr != NULL) {
+            printf("Error: %s\n", error_ptr);
+        }
+        cJSON_Delete(json);
+        return 1;
+    }
+
+}
